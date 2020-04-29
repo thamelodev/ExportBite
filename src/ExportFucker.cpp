f3a7edaa1857d2efbd6270985b237f7274ed5d6c
Julimar Melo <melobrdev@gmail.com>
2020-04-29 12:13:06 -0300

    Hotfix v2

--- src/ExportFucker.cpp
+++ src/ExportFucker.cpp
@@ -35,11 +35,11 @@ bool export_fucker::set_target_module( std::string module_name )
 	export_fucker::nt_headers = reinterpret_cast< PIMAGE_NT_HEADERS >( ( char* ) export_fucker::dos_header + export_fucker::dos_header->e_lfanew );
 
 	// Gets Optional Header
-	export_fucker::op_header  = &export_fucker::nt_headers->OptionalHeader;
+	export_fucker::op_header = &export_fucker::nt_headers->OptionalHeader;
 
 
 	// Gets Data Directory
-	export_fucker::data_dir   = export_fucker::op_header->DataDirectory;
+	export_fucker::data_dir = export_fucker::op_header->DataDirectory;
 
 	// Gets Export Directory Table
 	export_fucker::export_dir = reinterpret_cast< PIMAGE_EXPORT_DIRECTORY >( ( char* ) export_fucker::dos_header + export_fucker::data_dir [ 0 ].VirtualAddress );
@@ -51,10 +51,10 @@ bool export_fucker::set_target_module( std::string module_name )
 	char text_sec_string [ 8 ] = { '.', 't', 'e', 'x', 't', '\x00', '\x00', '\x00' };
 
 	// Find the .text section header
-	for ( size_t i = 0; i < export_fucker::nt_headers->FileHeader.NumberOfSections ; ++i )
+	for ( size_t i = 0; i < export_fucker::nt_headers->FileHeader.NumberOfSections; ++i )
 	{
 		// The section name is UTF-8 Null padded string with 8 characters, i'm comparing if the section name is .text
-		if ( memcmp(&export_fucker::sec_header->Name, text_sec_string, 8  ) == 0)
+		if ( memcmp( &export_fucker::sec_header->Name, text_sec_string, 8 ) == 0 )
 			break;
 
 		// Walks on the section headers
@@ -68,11 +68,11 @@ bool export_fucker::set_target_module( std::string module_name )
 bool export_fucker::hook( uintptr_t hook_addr, std::string function_name )
 {
 	// Gets Name Pointer Table
-	const auto               name_rva			  = *reinterpret_cast< uintptr_t* >( ( char* ) export_fucker::dos_header + export_fucker::export_dir->AddressOfNames );
+	const auto               name_rva = *reinterpret_cast< uintptr_t* >( ( char* ) export_fucker::dos_header + export_fucker::export_dir->AddressOfNames );
 	// Gets Name Pointer Table Length
-	const auto			     name_length	      = export_fucker::export_dir->NumberOfNames;
+	const auto			     name_length = export_fucker::export_dir->NumberOfNames;
 	// The index will be the value that we can use on Export Address Table
-	int					     index                = { 0 };
+	int					     index = { 0 };
 	// Gets Export Address Table
 	auto					 export_address_table = reinterpret_cast< uintptr_t* >( ( char* ) export_fucker::dos_header + export_fucker::export_dir->AddressOfFunctions );
 
@@ -92,7 +92,7 @@ bool export_fucker::hook( uintptr_t hook_addr, std::string function_name )
 		// If the functiom name wasn't found
 		if ( buffer.length( ) < name_length )
 			return false;
-		
+
 		//Increases the index that will be very important
 		++index;
 	}
@@ -113,9 +113,9 @@ bool export_fucker::hook( uintptr_t hook_addr, std::string function_name )
 	// If there's no place return false
 	if ( !code_cave_addr )
 		return false;
-	
+
 	// PUSH + ADDDRESS  + RET Shellcode intialized
-	char shellcode [ ] = { '\x68', '\x00', '\x00', '\x00', '\x00', '\xC3' };
+	char shellcode [ ] = { '\xB8', '\x00', '\x00', '\x00', '\x00', '\xFF', '\xD0', '\xC3' };
 
 	// Place the address on shellcode
 	memcpy( shellcode + 1, &hook_addr, sizeof( int32_t ) );
@@ -128,13 +128,13 @@ bool export_fucker::hook( uintptr_t hook_addr, std::string function_name )
 		return false;
 
 	// Copy the shellcode to code_cave
-	memcpy( reinterpret_cast< void* >( code_cave_addr ), shellcode, 6 );
+	memcpy( reinterpret_cast< void* >( code_cave_addr ), shellcode, 8 );
 
 	// Change the protection to EXECUTE because I'll let the .text as it was before.
 	if ( !VirtualProtect( reinterpret_cast< char* >( export_fucker::dos_header ), export_fucker::module_size, PAGE_EXECUTE, &old_protect ) )
 		return false;
 
-	
+
 	// Calculate code cade rva
 	uintptr_t code_cave_rva = code_cave_addr - reinterpret_cast< uintptr_t >( export_fucker::dos_header );
 
@@ -147,7 +147,7 @@ bool export_fucker::hook( uintptr_t hook_addr, std::string function_name )
 	// Set the address range protection for what it's before.
 	if ( !VirtualProtect( reinterpret_cast< void* >( export_address_table ), sizeof( int32_t ), old_protect, &old_protect ) )
 		return false;
-	
+
 	// return true because the hook was a success
 	return true;
 }
@@ -159,18 +159,18 @@ uintptr_t export_fucker::find_code_cave( )
 	DWORD old_protect = { 0 };
 
 	// Set the .text section protection for read, write and execute because i'll need to READ the opcodes and search for 5 null-bytes
-	if(!VirtualProtect(reinterpret_cast<char*>( export_fucker::dos_header ), export_fucker::module_size,  PAGE_EXECUTE_READWRITE,  &old_protect))
+	if ( !VirtualProtect( reinterpret_cast< char* >( export_fucker::dos_header ), export_fucker::module_size, PAGE_EXECUTE_READWRITE, &old_protect ) )
 		return false;
 
 
 	// Find a code cave for place PUSH + ADDRESS + RET
-	for ( size_t i = export_fucker::module_size + reinterpret_cast<int32_t>(export_fucker::dos_header); i > 0; i -= 5 )
+	for ( size_t i = export_fucker::module_size + reinterpret_cast< int32_t >( export_fucker::dos_header ); i > 0; i -= 5 )
 	{
 		// Intializes the mem buffer
 		char mem_bytes [ 5 ] = { 0 };
 
 		// Shellcode with 5 null-bytes XD idk why i did this
-		char shellcode [ 6 ] = { '\x00', '\x00', '\x00', '\x00', '\x00', '\x00' };
+		char shellcode [ 8 ] = { '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00' };
 
 		// Copy the memory area to the mem buffer
 		memcpy( &mem_bytes, reinterpret_cast< char* >( i ), 5 );
@@ -179,7 +179,7 @@ uintptr_t export_fucker::find_code_cave( )
 		if ( memcmp( &mem_bytes, &shellcode, 5 ) == 0 )
 		{
 			// Set the .text section protection for what it's before.
-			if ( !VirtualProtect( reinterpret_cast< int32_t* >( export_fucker::dos_header ), export_fucker::module_size, PAGE_EXECUTE, &old_protect) )
+			if ( !VirtualProtect( reinterpret_cast< int32_t* >( export_fucker::dos_header ), export_fucker::module_size, PAGE_EXECUTE, &old_protect ) )
 				return false;
 
 			// returns code cave address
